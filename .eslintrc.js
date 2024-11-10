4323df25d222ca08e31a3fc910b67278ded61189
Sutej Pal <sutejpal@hotmail.com>
2024-11-11 01:01:56 +0530

    added eslint prettier and husky Updated package json to latest package versions

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/self-closing-comp": "off",
+    "no-constant-condition": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "warn",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
